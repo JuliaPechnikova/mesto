9113817b93b3c1a3f9e4990ac84a243cf1850ab3
Julia Pechnikova <juliape4nikova@yandex.ru>
2021-09-06 22:59:17 +0300

    hotfix/popup-script

--- scripts/script.js
+++ scripts/script.js
@@ -1,64 +1,62 @@
 let content = document.querySelector('.content');
 let editButton = content.querySelector('.profile__edit-button');
-let popup = content.querySelector('.popup');
 let open = false;
 let personName = content.querySelector('.profile__name');
 let description = content.querySelector('.profile__description');
 
-
 //Добавление формы редактирования Имени и профессии пользователя
 
-  function openedPopup() {
+content.insertAdjacentHTML('beforeend', `
+<div class="popup">
+  <div class="popup__container">
+  <h2 class="popup__header">Редактировать профиль</h2>
+  <form class="popup__texts" name="formdata" action="#" method="POST">
+    <input type="text" value="Жак-Ив Кусто" placeholder="Имя" class="popup__text" id="name" name="name">
+    <input type="text" value="Исследователь океана" placeholder="О себе" class="popup__text" id="description" name="description">
+    <button type="submit" class="popup__save-btn">Сохранить</button>
+  </form>
+  <button class="popup__close-btn" type="reset">
+    <img class="popup__close-btn-image" src="./images/edit-form-image.svg" alt="X">
+  </button>
+  </div>
+</div>`);
 
-    //Добавление класса для отображения попап
-    popup.classList.add('popup_opened');
+//Объявление переменных после внесения текста попапа в тело main 
+let popup = content.querySelector('.popup');
+let closeButton = popup.querySelector('.popup__close-btn');
+let nameInput = popup.querySelector('#name');
+let descriptionInput = popup.querySelector('#description');
+let form = popup.querySelector('.popup__texts');
 
-    //Если попап был прописан, то повторно добавлятся не будет
-    if (open === !true) {
-      popup.insertAdjacentHTML('beforeend', `
-      <div class="popup__container">
-      <h2 class="popup__header">Редактировать профиль</h2>
-      <form class="popup__texts" name="formdata" action="#" method="POST">
-        <input type="text" value="${personName.textContent}" placeholder="Имя" class="popup__text" id="name" name="name">
-        <input type="text" value="${description.textContent}" placeholder="О себе" class="popup__text" id="description" name="description">
-        <button type="submit" class="popup__save-btn">Сохранить</button>
-      </form>
-      <button class="popup__close-btn" type="reset">
-        <img class="popup__close-btn-image" src="./images/edit-form-image.svg" alt="X">
-      </button>
-      </div>`);
+// Функция открытия попап
+function openedPopup() {
+  //Добавление класса для отображения попап
+  popup.classList.add('popup_opened');
+      
+  //Добавление данных из профиля в форму
+  nameInput.value = `${personName.textContent}`;
+  descriptionInput.value = `${description.textContent}`;
+};
 
-      open = true;
-    };
+//Функция сохранения данных, введенных в форму
+function submitForm(evt) {
+  evt.preventDefault(); 
+  personName.textContent = nameInput.value;
+  description.textContent = descriptionInput.value; 
+  closedPopup(); //Закрываем форму после редактирования
+};
 
-    //Объявление переменных формы
-    let nameInput = popup.querySelector('#name');
-    let descriptionInput = popup.querySelector('#description');
+//Функция закрытия попап
+function closedPopup() {
+  popup.classList.remove('popup_opened');
+};
 
-    //Добавляем дефолтные значения, для того, чтобы сбрасывались данные при закрытии без отправки
-    nameInput.value = `${personName.textContent}`;
-    descriptionInput.value = `${description.textContent}`;
-  
-    //Функция сохранения данных, введенных в форму
-    function submitForm(evt) {
-      evt.preventDefault(); 
-      personName.textContent = nameInput.value;
-      description.textContent = descriptionInput.value; 
-      closedPopup(); //Закрываем форму после редактирования
-    };
+//Слушатель на открытие попап
+editButton.addEventListener('click', openedPopup);
 
-    let closeButton = popup.querySelector('.popup__close-btn');
+//Слушатель на форму при отправке
+form.addEventListener('submit', submitForm);  
 
-    //Слуушатель на форму при отправке
-    popup.addEventListener('submit', submitForm);  
-
-    //Закрытие попапа реализуется удалением класса
-    function closedPopup() {
-      popup.classList.remove('popup_opened');
-    };
-
-    closeButton.addEventListener('click', closedPopup);
-  };
-
-  editButton.addEventListener('click', openedPopup);
+//Слушатель на закрытие попап
+closeButton.addEventListener('click', closedPopup);
 
